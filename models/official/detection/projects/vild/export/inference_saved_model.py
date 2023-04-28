53c285da4c47676b8bf52b2d18208449c5e6d516
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2023-04-28 20:31:21 +0200

    Inference saved model seems entirely working now (ViLD)

--- models/official/detection/projects/vild/export/inference_saved_model.py
+++ models/official/detection/projects/vild/export/inference_saved_model.py
@@ -26,8 +26,9 @@ flags.DEFINE_string('saved_model_dir', None, "Saved model directory to inference
 flags.DEFINE_string('class_map', None, "LVIS class ID to name mapping file (each line contains e.g. '19:armchair', IDs are 1-indexed, should be 1203 of them)")
 flags.DEFINE_multi_string('image', None, "Input image file path(s)")
 flags.DEFINE_boolean('graph_opt', True, "Perform graph optimisation on loaded model")
-flags.DEFINE_integer('max_boxes', 50, "Maximum number of RoI/detection boxes to draw")
-flags.DEFINE_float('min_score_roi', 0.2, "Minimum score in order to draw a RoI box")
+flags.DEFINE_integer('max_boxes_roi', 100, "Maximum number of RoI boxes to draw")
+flags.DEFINE_integer('max_boxes_det', 50, "Maximum number of detection boxes to draw")
+flags.DEFINE_float('min_score_roi', 0.9, "Minimum score in order to draw a RoI box")
 flags.DEFINE_float('min_score_det', 0.2, "Minimum score in order to draw a detection box")
 flags.mark_flag_as_required('saved_model_dir')
 flags.mark_flag_as_required('image')
@@ -40,7 +41,8 @@ def main(argv):
 		class_map_path=FLAGS.class_map,
 		image_paths=FLAGS.image,
 		graph_opt=FLAGS.graph_opt,
-		max_boxes=FLAGS.max_boxes,
+		max_boxes_roi=FLAGS.max_boxes_roi,
+		max_boxes_det=FLAGS.max_boxes_det,
 		min_score_roi=FLAGS.min_score_roi,
 		min_score_det=FLAGS.min_score_det,
 	)
@@ -51,7 +53,8 @@ def inference(
 	class_map_path,
 	image_paths,
 	graph_opt,
-	max_boxes,
+	max_boxes_roi,
+	max_boxes_det,
 	min_score_roi,
 	min_score_det,
 ):
@@ -68,7 +71,8 @@ def inference(
 		for image_path in image_paths:
 			print(f"    {image_path}")
 	print(f"  Optimise graph: {graph_opt}")
-	print(f"  Max draw boxes: {max_boxes}")
+	print(f"  Max RoI boxes: {max_boxes_roi}")
+	print(f"  Max det boxes: {max_boxes_det}")
 	print(f"  Min RoI score: {min_score_roi}")
 	print(f"  Min det score: {min_score_det}")
 	print()
@@ -176,7 +180,7 @@ def inference(
 					print(f"RPN RoI boxes format: [ymin xmin ymax xmax] relative to {input_width}x{input_height} input image")
 					print(f"RPN RoI boxes min: {rpn_roi_boxes.min(axis=0)}")
 					print(f"RPN RoI boxes max: {rpn_roi_boxes.max(axis=0)}")
-					# TODO: Use rpn_roi_scores and show image
+					show_annotations(title='Regions of interest', image_bgr=image_bgr, boxes=rpn_roi_boxes, scores=rpn_roi_scores, max_boxes=max_boxes_roi, min_score=min_score_roi)
 					print()
 
 				if (rpn_roi_features := output_results.get('rpn_roi_features', None)[0]) is not None:
@@ -251,40 +255,44 @@ def inference(
 					print(f"Det boxes min: {det_boxes.min(axis=0)}")
 					print(f"Det boxes max: {det_boxes.max(axis=0)}")
 					instance_masks = utils.mask_utils.paste_instance_masks(det_masks, utils.box_utils.yxyx_to_xywh(det_boxes), input_height, input_width) if det_masks is not None else None
-					cv2.imshow('Detections', utils.object_detection.visualization_utils.visualize_boxes_and_labels_on_image_array(
-						image=image_bgr.copy(),
-						boxes=det_boxes,
-						classes=det_classes,
-						agnostic_mode=det_classes is None,
-						scores=det_scores if det_scores is not None else np.ones(det_boxes.shape[0], dtype=np.float32),
-						category_index=class_map,
-						instance_masks=instance_masks,
-						use_normalized_coordinates=False,
-						max_boxes_to_draw=max_boxes,
-						min_score_thresh=min_score_det,
-						line_thickness=LINE_THICKNESS,
-					))
-					num_boxes = det_boxes.shape[0]
-					if det_scores is None:
-						if num_boxes <= max_boxes:
-							print(f"Annotated {num_boxes} det boxes")
-						else:
-							print(f"Annotated {max_boxes} (limit) det boxes")
-					else:
-						scored_boxes = np.searchsorted(-det_scores, -min_score_det, side='left')
-						if scored_boxes <= 0:
-							print(f"Annotated 0 det boxes (none are above score of {min_score_det:.1%})")
-						elif scored_boxes > max_boxes:
-							print(f"Annotated {max_boxes} (limit) det boxes down to a score of {det_scores[scored_boxes - 1]:.1%}")
-						elif scored_boxes < num_boxes:
-							print(f"Annotated {scored_boxes} det boxes down to a score of {det_scores[scored_boxes - 1]:.1%} (limit)")
-						else:
-							print(f"Annotated {scored_boxes} det boxes down to a score of {det_scores[scored_boxes - 1]:.1%}")
+					show_annotations(title='Object detections', image_bgr=image_bgr, boxes=det_boxes, scores=det_scores, classes=det_classes, class_map=class_map, instance_masks=instance_masks, max_boxes=max_boxes_det, min_score=min_score_det)
 					print()
 
 			cv2.waitKey(0)
 
 		cv2.destroyAllWindows()
+
+# Show an annotated image of bounding boxes
+def show_annotations(title, image_bgr, boxes, scores, max_boxes, min_score, classes=None, class_map=None, instance_masks=None):
+	cv2.imshow(title, utils.object_detection.visualization_utils.visualize_boxes_and_labels_on_image_array(
+		image=image_bgr.copy(),
+		boxes=boxes,
+		classes=classes,
+		agnostic_mode=classes is None,
+		scores=scores if scores is not None else np.ones(boxes.shape[0], dtype=np.float32),
+		category_index=class_map,
+		instance_masks=instance_masks,
+		use_normalized_coordinates=False,
+		max_boxes_to_draw=max_boxes,
+		min_score_thresh=min_score,
+		line_thickness=LINE_THICKNESS,
+	))
+	num_boxes = boxes.shape[0]
+	if scores is None:
+		if num_boxes <= max_boxes:
+			print(f"Annotated {num_boxes} boxes")
+		else:
+			print(f"Annotated {max_boxes} (limit) boxes")
+	else:
+		scored_boxes = np.searchsorted(-scores, -min_score, side='left')
+		if scored_boxes <= 0:
+			print(f"Annotated 0 boxes (none are above score of {min_score:.1%})")
+		elif scored_boxes > max_boxes:
+			print(f"Annotated {max_boxes} (limit) boxes down to a score of {scores[max_boxes - 1]:.1%}")
+		elif scored_boxes < num_boxes:
+			print(f"Annotated {scored_boxes} boxes down to a score of {scores[scored_boxes - 1]:.1%} (limit)")
+		else:
+			print(f"Annotated {scored_boxes} boxes down to a score of {scores[scored_boxes - 1]:.1%}")
 
 # Shape to string
 def shape_str(array):
